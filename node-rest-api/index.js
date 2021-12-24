2e40f879eaa1e7111295a9d02fa3ec109fc758c5
Heng <165647619@qq.com>
2021-12-24 23:51:10 +1100

    created database schema and tested using postma"

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,12 +4,29 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
+//connection
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
-  console.log("connected to MongoDB");
-});
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
 
-app.listen(8890, () => {
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
   console.log("backend server is running!!");
 });
