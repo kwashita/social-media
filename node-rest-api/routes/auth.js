94b7eb6bcf7ffd2698e4699e48795eb3393912a9
Heng <165647619@qq.com>
2021-12-26 08:09:47 +1100

    login has been added

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 //register
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    //save user and  respond
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
@@ -16,4 +21,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;
